23096bdd0550a01f5cd894902437635771d5056e
capitansmolet <86357990+capitansmolet@users.noreply.github.com>
2021-06-23 16:21:21 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,10 @@
+  
 import time
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_should_have_button(browser):
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_basket_button(browser):
     browser.get(link)
-    button=browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert button.text=="Añadir al carrito"
+    time.sleep(30)
+    add_button = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert add_button , "button is not on the page"
