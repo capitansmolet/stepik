1409837cf09dd57fa6e11f35bc8ef0d546a5a825
capitansmolet <86357990+capitansmolet@users.noreply.github.com>
2021-06-24 13:54:25 +0300

    Delete locators.py

--- pages/locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from pages.main_page import *
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-class LoginPageLocators():
-    Registration_link = (By.XPATH, "//input[@name='registration-redirect_url']")
